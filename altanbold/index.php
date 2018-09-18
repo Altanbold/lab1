35338c574eb7aaf2363011c2fc63130be1c8313c
Altanbold <altanbold0909@gmail.com>
2018-09-18 16:32:27 +0800

    lab 1 deer nemelt oruulav

--- altanbold/index.php
+++ altanbold/index.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<!-- index.php -->
-<html>
-  <head>
-   <meta charset="UTF-8">
-  </head><title>Hello my lab1</title>
-  <body>
-    <!-- Статик болон динамик агуулгын ялгаа -->
-    <p font-color:"green" >Энэ бол статик агуулга.</p>
+<?php 
     
-    <?php echo "PHP скриптээр, програмын кодоор үүсгэсэн динамик агуулга."; ?>
-    <p>Вэб серверийн цаг: 
-       <span><?php 
-              // Системийн цагийг HTTP гаралтад бичих
-              echo "Өнөөдөр бол " . date("Y/m/d");
-             ?></span>
-    <p>
-  </body>
-</html>
+    for($i=0; $i<100;$i++)
+
+      echo "jawhlan\n";
+    
+    ?>
